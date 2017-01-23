bbb04e656fbb9103c8ee2dccd28adec21a951d62
Ione Souza Junior <junior@ionixjunior.com.br>
2017-01-23 21:28:19 -0200

    Adicionado view model locator e teste de data binding na home

---
+++ Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Core.ViewModels
+{
+	public class MainViewModel
+	{
+		public MainViewModel()
+		{
+		}
+	}
+}
---
+++ Core/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Core.ViewModels
+{
+	public class ViewModelLocator
+	{
+		public ViewModelLocator()
+		{
+		}
+	}
+}
