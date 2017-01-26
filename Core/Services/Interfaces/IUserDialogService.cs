13a68b463dd19e7bad89eeab0d4f8c4a7a56ebde
Ângelo Polotto <angelopolotto@hotmail.com>
2017-01-25 22:30:45 -0200

    IUserDialogs com implementação iniciada

---
+++ Core/Services/Interfaces/IUserDialogService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services.Interfaces
+{
+    public enum AlertClickedAction
+    {
+        Cancel = 0,
+        Ok = 1,
+    }
+
+    public class AlertCallback
+    {
+        public AlertClickedAction AlertClickedAction { get; set; }
+        public string Text { get; set; }
+    }
+
+    public class AlertParameters
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+
+        public string OkButton { get; set; }
+        public string CancelButton { get; set; }
+
+        public bool HideOkButton { get; set; }
+        public bool HideCancelButton { get; set; }
+    }
+
+    public interface IUserDialogService
+    {
+        void ShowToast(string message);
+        Task<bool> ConfirmAsync(string message, string title = "");
+        Task<AlertCallback> AlertAsync(AlertParameters parms);
+        Task<AlertCallback> AlertAsync(string title, string msg);
+    }
+}
---
+++ Core/Services/UserDialogService.cs
@@ -0,0 +1,32 @@
+﻿using Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class UserDialogService : IUserDialogService
+    {
+        public Task<AlertCallback> AlertAsync(AlertParameters parms)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AlertCallback> AlertAsync(string title, string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ConfirmAsync(string message, string title = "")
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowToast(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
